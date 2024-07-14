4ab16be57600508ede32efce0b53b4e2389f8b8a
underbrain <knarfoo1313@gmail.com>
2024-07-14 01:35:14 -0700

    Update package name and version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.underbrain.find-four-twenty"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
 
     compileOptions {
